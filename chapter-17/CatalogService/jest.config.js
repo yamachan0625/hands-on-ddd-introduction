f48b350cfb99b1e1f9b826b4501849ae52b617e1
yamachan0625 <55592297+yamachan0625@users.noreply.github.com>
2025-10-12 17:18:05 +0900

    maxWorkers: 1

--- chapter-17/CatalogService/jest.config.js
+++ chapter-17/CatalogService/jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  maxWorkers: 1,
   moduleDirectories: ["node_modules", "src"],
   transformIgnorePatterns: ["/node_modules"],
 };
