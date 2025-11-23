289da2f357d081d3db9af4c04e768276a3a6f3f6
yamachan0625 <55592297+yamachan0625@users.noreply.github.com>
2025-11-23 13:46:21 +0900

    fix MockDomainEventPublisher

--- chapter-21/CatalogService/src/Application/shared/DomainEvent/MockDomainEventPublisher.ts
+++ chapter-21/CatalogService/src/Application/shared/DomainEvent/MockDomainEventPublisher.ts
@@ -3,7 +3,9 @@ import { DomainEvent } from "Domain/shared/DomainEvent/DomainEvent";
 import { IDomainEventPublisher } from "./IDomainEventPublisher";
 
 export class MockDomainEventPublisher implements IDomainEventPublisher {
-  publish(domainEvent: DomainEvent) {
-    return domainEvent;
+  private publishedEvents: DomainEvent[] = [];
+
+  publish(domainEvent: DomainEvent): void {
+    this.publishedEvents.push(domainEvent);
   }
 }
